42d9d19824888c9b1bb42d67ff3b8d6554774727
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2021-03-08 20:41:12 +0300

    Initial Commit

---
+++ ApiServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dawnstar\Api;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+
+
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
+
